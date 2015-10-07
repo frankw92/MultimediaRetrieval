cf8819f1ac57ed75b5b5e7fe1ed8c65f2142d64e
Frank Wagenmans <frank.wagenmans@gmail.com>
2015-10-07 19:41:31 +0200

    minor fixes

--- MultimediaRetrieval/Main.cs
+++ MultimediaRetrieval/Main.cs
@@ -12,41 +12,47 @@ using NAudio.Wave;
 
 namespace WindowsFormsApplication1
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
         WaveIn waveIn;
         WaveOut waveOut;
         BufferedWaveProvider bufferedWaveProvider;
         bool recording, playing;
 
-        public Form1()
+        public Main()
         {
             InitializeComponent();
-            button2.Visible = false;
+            playButton.Visible = false;
             waveIn = new WaveIn();
             WaveFormat waveFormat = new WaveFormat(44100, 1);
             waveIn.WaveFormat = waveFormat;
-            waveIn.DataAvailable += new EventHandler<WaveInEventArgs>(dataAvailable);
+            waveIn.DataAvailable += dataAvailable;
 
             bufferedWaveProvider = new BufferedWaveProvider(waveFormat);
-
+            bufferedWaveProvider.BufferDuration = new TimeSpan(0,0,20); //20 seconds
+            bufferedWaveProvider.DiscardOnBufferOverflow = true;
             waveOut = new WaveOut();
-            waveOut.Init(bufferedWaveProvider);
-
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        
+
+        private void recordButton_Click(object sender, EventArgs e)
         {
             if (!recording)
             {
-                button1.Text = "Stop Recording";
+                bufferedWaveProvider.ClearBuffer();
+                waveOut.Init(bufferedWaveProvider);
+
+                playButton.Visible = false;
+
+                recordButton.Text = "Stop Recording";
                 waveIn.StartRecording();
                 recording = true;
             }
             else
             {
-                button2.Visible = true;
-                button1.Text = "Record";
+                playButton.Visible = true;
+                recordButton.Text = "Record";
                 waveIn.StopRecording();
                 recording = false;
             }
@@ -57,25 +63,37 @@ namespace WindowsFormsApplication1
             bufferedWaveProvider.AddSamples(e.Buffer, 0, e.BytesRecorded);
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Main_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void playButton_Click(object sender, EventArgs e)
         {
             if (!playing)
             {
-                playing = true;
-                button2.Text = "Stop";
-                waveOut.Play();
+                StartPlaying();
             }
             else
             {
-                playing = false;
-                button2.Text = "Play";
-                waveOut.Stop();
+                StopPlaying();
             }
+        }
+
+        private void StartPlaying()
+        {
+            playing = true;
+            playButton.Text = "Stop";
+            recordButton.Visible = false;
+            waveOut.Play();
+        }
+
+        private void StopPlaying()
+        {
+            playing = false;
+            recordButton.Visible = true;
+            playButton.Text = "Play";
+            waveOut.Stop();
         }
     }
 }
