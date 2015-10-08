7174abadd4172a3925fba3499187bc83ecf12750
Frank Wagenmans <frank.wagenmans@gmail.com>
2015-10-08 12:19:59 +0200

    niek deed het beter

---
+++ TestAudioForm/TestAudioForm/TestAudio.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace TestAudioForm
+{
+    partial class TestAudio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startRecordingButton = new System.Windows.Forms.Button();
+            this.stopRecordingButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startRecordingButton
+            // 
+            this.startRecordingButton.Location = new System.Drawing.Point(12, 12);
+            this.startRecordingButton.Name = "startRecordingButton";
+            this.startRecordingButton.Size = new System.Drawing.Size(260, 23);
+            this.startRecordingButton.TabIndex = 0;
+            this.startRecordingButton.Text = "Start Recording";
+            this.startRecordingButton.UseVisualStyleBackColor = true;
+            this.startRecordingButton.Click += new System.EventHandler(this.startRecordingButton_Click);
+            // 
+            // stopRecordingButton
+            // 
+            this.stopRecordingButton.Location = new System.Drawing.Point(12, 41);
+            this.stopRecordingButton.Name = "stopRecordingButton";
+            this.stopRecordingButton.Size = new System.Drawing.Size(260, 23);
+            this.stopRecordingButton.TabIndex = 1;
+            this.stopRecordingButton.Text = "Stop Recording";
+            this.stopRecordingButton.UseVisualStyleBackColor = true;
+            this.stopRecordingButton.Click += new System.EventHandler(this.stopRecordingButton_Click);
+            // 
+            // TestAudio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 77);
+            this.Controls.Add(this.stopRecordingButton);
+            this.Controls.Add(this.startRecordingButton);
+            this.Name = "TestAudio";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startRecordingButton;
+        private System.Windows.Forms.Button stopRecordingButton;
+    }
+}
+
---
+++ TestAudioForm/TestAudioForm/TestAudio.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+
+
+namespace TestAudioForm
+{
+    public partial class TestAudio : Form
+    {
+        private WaveIn waveIn;
+        private BufferedWaveProvider bufferedWaveProvider;
+        private short[] waveArray;
+        private int arrayPointer;
+
+
+        public TestAudio()
+        {
+            InitializeComponent();
+
+            waveArray = new short[10000000];
+            arrayPointer = 0;
+        }
+
+
+        public void NewDataAvailable(object sender, WaveInEventArgs e)
+        {
+            //Shit hier als er nieuwe data is!!!
+            for (int i = 0; i < e.BytesRecorded; i += 2)
+            {
+
+                short sample = (short)((e.Buffer[i + 1] << 8) | e.Buffer[i + 0]);
+      
+                waveArray[arrayPointer] = sample;
+                arrayPointer = (arrayPointer + 1) % waveArray.Length;
+            }
+
+        }
+
+        private void startRecordingButton_Click(object sender, EventArgs e)
+        {
+            arrayPointer = 0;
+            waveIn = new WaveIn();
+            waveIn.DataAvailable += new EventHandler<WaveInEventArgs>(NewDataAvailable);
+            bufferedWaveProvider = new BufferedWaveProvider(waveIn.WaveFormat);
+
+            try
+            {
+                waveIn.StartRecording();
+            }
+            catch
+            {
+                MessageBox.Show("Ja echt dom om te recorden zonder microfoon...");
+            }
+        }
+
+        private void stopRecordingButton_Click(object sender, EventArgs e)
+        {
+            int x = ZeroCrossings();
+            waveIn.StopRecording();
+            waveIn.Dispose();
+            waveIn = null;
+        }
+
+        public int ZeroCrossings()
+        {
+            if (arrayPointer == 0)
+                return 0;
+
+
+            int start = 0;
+
+            while (waveArray[start] == 0 && start <= arrayPointer)
+            {
+                start++;
+            }
+
+            if (start == arrayPointer)
+                return 0;
+
+            short current = waveArray[start];
+
+            int crossings = 0;
+
+            for (int i = 1; i <= arrayPointer; i++)
+            {
+                if (waveArray[i] != 0 && (waveArray[i] * current) < 0)
+                {
+                    crossings++;
+                    current = waveArray[i];
+                }
+            }
+
+            return crossings / waveIn.WaveFormat.SampleRate;
+        }
+    }
+}
