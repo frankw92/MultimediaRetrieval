73e49cbff22c1211f6c61b3e2fea99d30135d690
nickdekkers <n.dekkers.91@gmail.com>
2015-10-13 15:10:45 +0200

    Database Manager

---
+++ TestAudioForm/TestAudioForm/DatabaseManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAudioForm
+{
+    class DatabaseManager
+    {
+        // Populate database
+            // Read wav files
+            
+            // Process wav files 
+                // Get values (pitch average, pitch standard deviation, energy standard deviation)
+                // Write to text file
+
+        // Read information from text file
+            // Put in data structure (List???)
+            // Functionality
+    }
+}
