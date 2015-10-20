adea83f5c56a271a999e71d48093dfdfc938a2e5
marijnefkens <marijnefkens@gmail.com>
2015-10-20 11:50:32 +0200

    Removed playlist button, functionality built in after stop recording

--- TestAudioForm/TestAudioForm/TestAudio.Designer.cs
+++ TestAudioForm/TestAudioForm/TestAudio.Designer.cs
@@ -28,15 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.startRecordingButton = new System.Windows.Forms.Button();
             this.stopRecordingButton = new System.Windows.Forms.Button();
             this.waveChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.musicButton = new System.Windows.Forms.Button();
             this.pauseButton = new System.Windows.Forms.Button();
-            this.playlistButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.waveChart)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,7 +44,7 @@
             this.startRecordingButton.Location = new System.Drawing.Point(16, 15);
             this.startRecordingButton.Margin = new System.Windows.Forms.Padding(4);
             this.startRecordingButton.Name = "startRecordingButton";
-            this.startRecordingButton.Size = new System.Drawing.Size(722, 23);
+            this.startRecordingButton.Size = new System.Drawing.Size(493, 23);
             this.startRecordingButton.TabIndex = 0;
             this.startRecordingButton.Text = "Start Recording";
             this.startRecordingButton.UseVisualStyleBackColor = true;
@@ -57,7 +56,7 @@
             this.stopRecordingButton.Location = new System.Drawing.Point(17, 50);
             this.stopRecordingButton.Margin = new System.Windows.Forms.Padding(4);
             this.stopRecordingButton.Name = "stopRecordingButton";
-            this.stopRecordingButton.Size = new System.Drawing.Size(721, 23);
+            this.stopRecordingButton.Size = new System.Drawing.Size(492, 23);
             this.stopRecordingButton.TabIndex = 1;
             this.stopRecordingButton.Text = "Stop Recording";
             this.stopRecordingButton.UseVisualStyleBackColor = true;
@@ -65,28 +64,28 @@
             // 
             // waveChart
             // 
-            chartArea2.Name = "ChartArea1";
-            this.waveChart.ChartAreas.Add(chartArea2);
-            legend2.Enabled = false;
-            legend2.Name = "Legend1";
-            this.waveChart.Legends.Add(legend2);
+            chartArea1.Name = "ChartArea1";
+            this.waveChart.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.waveChart.Legends.Add(legend1);
             this.waveChart.Location = new System.Drawing.Point(17, 87);
             this.waveChart.Margin = new System.Windows.Forms.Padding(4);
             this.waveChart.Name = "waveChart";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.waveChart.Series.Add(series2);
-            this.waveChart.Size = new System.Drawing.Size(721, 533);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.waveChart.Series.Add(series1);
+            this.waveChart.Size = new System.Drawing.Size(628, 473);
             this.waveChart.TabIndex = 2;
             this.waveChart.Text = "waveChart";
             // 
             // musicButton
             // 
-            this.musicButton.Location = new System.Drawing.Point(746, 50);
+            this.musicButton.Location = new System.Drawing.Point(537, 13);
             this.musicButton.Margin = new System.Windows.Forms.Padding(4);
             this.musicButton.Name = "musicButton";
-            this.musicButton.Size = new System.Drawing.Size(129, 23);
+            this.musicButton.Size = new System.Drawing.Size(108, 23);
             this.musicButton.TabIndex = 4;
             this.musicButton.Text = "Play Next Song";
             this.musicButton.UseVisualStyleBackColor = true;
@@ -94,30 +93,19 @@
             // 
             // pauseButton
             // 
-            this.pauseButton.Location = new System.Drawing.Point(746, 87);
+            this.pauseButton.Location = new System.Drawing.Point(537, 50);
             this.pauseButton.Name = "pauseButton";
-            this.pauseButton.Size = new System.Drawing.Size(129, 23);
+            this.pauseButton.Size = new System.Drawing.Size(108, 23);
             this.pauseButton.TabIndex = 5;
             this.pauseButton.Text = "Pause/Play Music";
             this.pauseButton.UseVisualStyleBackColor = true;
             this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
             // 
-            // playlistButton
-            // 
-            this.playlistButton.Location = new System.Drawing.Point(746, 15);
-            this.playlistButton.Name = "playlistButton";
-            this.playlistButton.Size = new System.Drawing.Size(127, 23);
-            this.playlistButton.TabIndex = 7;
-            this.playlistButton.Text = "Create Playlist";
-            this.playlistButton.UseVisualStyleBackColor = true;
-            this.playlistButton.Click += new System.EventHandler(this.playlistButton_Click);
-            // 
             // TestAudio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(885, 616);
-            this.Controls.Add(this.playlistButton);
+            this.ClientSize = new System.Drawing.Size(664, 580);
             this.Controls.Add(this.pauseButton);
             this.Controls.Add(this.musicButton);
             this.Controls.Add(this.waveChart);
@@ -141,7 +129,6 @@
         public System.Windows.Forms.Button stopRecordingButton;
         private System.Windows.Forms.Button musicButton;
         private System.Windows.Forms.Button pauseButton;
-        private System.Windows.Forms.Button playlistButton;
     }
 }
 
